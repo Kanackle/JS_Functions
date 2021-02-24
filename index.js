3e9ce3b021c658f4735638e6a2ab44f34feacb1f
xshi0603 <xshi603@gmail.com>
2021-02-23 22:48:01 -0500

    finished mySome

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ Array.prototype.myEach = function(callbackfn) {
     for(let i = 0; i < this.length; ++i) {
         callbackfn(this[i], i, this);
     }
+}
 /**
  * Performs the specified action for each element in an array.
  * @param callbackfn  A function that calls the callbackfn function one time for each element in the array.
@@ -37,6 +38,23 @@ Array.prototype.myFilter = function(callbackfn) {
     return returnArray;
 }
 
+/*
+some() (aka any())
+Without using the native “Array.prototype.some” method of JavaScript, 
+compose a function titled “mySome” that will take in an array of elements 
+and execute a callback function on each of those elements.
+*/
+Array.prototype.mySome =  function(callbackfn) {
+    for (let i = 0; i < this.length; i++) {
+        if (callbackfn(this[i], i, this)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
 /**
  * Main function to run tests in
  */
@@ -47,13 +65,20 @@ function main() {
     console.log(arr.forEach(x => console.log(x)));
     console.log(arr.myEach(x => console.log(x)));
 
-    // // test for myMap()
+    // test for myMap()
     console.log(arr.map(x => x * 2));
     console.log(arr.myMap(x => x * 2));
 
-    // // test for myFilter()
+    // test for myFilter()
     console.log(arr.filter(x => x > 2));
     console.log(arr.myFilter(x => x > 2));
+
+    // test for mySome()
+    console.log(arr.some(x => x % 2 == 0))
+    console.log(arr.mySome(x => x % 2 == 0))
+
+
+
 };
 
 main();
