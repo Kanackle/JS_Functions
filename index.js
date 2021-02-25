eb6b0387e5b9ae1f81cd56e82b69ab25a425cb45
Aamer Shikari <shikariaamer@gmail.com>
2021-02-25 03:19:50 -0500

    completion of myIncludes, myIndexOf, and myPush()

--- index.js
+++ index.js
@@ -81,7 +81,56 @@ Array.prototype.myReduce =  function(callbackfn) {
     }
     return retVal;
 }
-
+/* 
+f7 myIncludes
+Without using the native “Array.prototype.includes” method of JavaScript, compose 
+a function titled “myIncludes” that will take in an array of elements and indicate 
+whether or not a target element is contained within the input array. This returns 
+a boolean.
+*/
+Array.prototype.myIncludes = function(target) {
+    for (let i = 0; i < this.length; i++){
+        if (target == this[i]){
+            return true;
+        }
+    }
+    return false;
+}
+/* 
+f8 myIndexOf 
+Without using the native “Array.prototype.indexOf” method of JavaScript, compose a 
+function titled “myIndexOf” that will take in an array of elements and returns the 
+index of the first encounter of a target element (if it is found) or -1 if that 
+element does not exist within the input array.
+*/
+Array.prototype.myIndexOf = function(target) {
+    for (let i = 0; i < this.length; i++){
+        if (target == this[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+/* 
+f9 myPush 
+Without using the native “Array.prototype.push” method of JavaScript, compose a 
+function titled “myPush” that will take in an array of elements as well as an 
+elementToAdd and append that element to the end of the array.
+*/
+Array.prototype.myPush = function(element) {
+    this[this.length] = element;
+    /*
+    newLength = this.length + 1; 
+    let newArr = Array.apply(null, Array(newLength)).map(function () {}); 
+    for (let i = 0; i < this.length; i++){
+        if (i = newArr.length - 1){
+            newArr[i] = element;
+        } else { 
+            newArr[i] = this[i];
+        }
+    }
+    */
+}
 /*
 f10
 Without using the native “Array.prototype.lastIndexOf” method of JavaScript, 
@@ -133,36 +182,84 @@ function main() {
     let arr = [1, 2, 3, 4];
 
     // test for myEach
+    console.log("myEach Test Results:");
+
     console.log(arr.forEach(x => console.log(x)));
     console.log(arr.myEach(x => console.log(x)));
+    console.log();
 
     // test for myMap()
+    console.log("myMap Test Results:");
+    
     console.log(arr.map(x => x * 2));
     console.log(arr.myMap(x => x * 2));
+    console.log();
 
     // test for myFilter()
+    console.log("myFilter Test Results:");
+
     console.log(arr.filter(x => x > 2));
     console.log(arr.myFilter(x => x > 2));
+    console.log();
 
     // test for mySome()
+    console.log("mySome Test Results:");
+
     console.log(arr.some(x => x % 2 == 0))
     console.log(arr.mySome(x => x % 2 == 0))
+    console.log();
 
     // test for myEvery()
+    console.log("myEvery Test Results:");
+
     console.log(arr.some(x => x % 2 == 0))
     console.log(arr.mySome(x => x % 2 == 0))
+    console.log();
 
     // test for myReduce()
+    console.log("myReduce Test Results:");
+
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     console.log(arr.reduce(reducer))
     console.log(arr.myReduce(reducer))
+    console.log();
+    
+    arr = [1,2,3,4];
+    
+    //test for myIncludes 
+    console.log("myIncludes Test Results:");
 
+    console.log(arr.myIncludes(1));
+    console.log(arr.myIncludes(3));
+    console.log(arr.myIncludes(5));
+    console.log();
 
-    let arr = [1, 2, 4, 4];
+    //test for myIndexOf()
+    console.log("myIndexOf Test Results:");
+
+    console.log(arr.myIndexOf(1));
+    console.log(arr.myIndexOf(3));
+    console.log(arr.myIndexOf(5));
+    console.log();
+
+    //test for myPush()
+    console.log("myPush Test Results:");
+
+    arr.myPush(5);
+    console.log(arr);
+    arr.myPush(6);
+    console.log(arr);
+    arr.myPush(7);
+    console.log(arr);
+    console.log();
+
+    arr = [1, 2, 4, 4];
 
     //test for lastIndexOf()
+    console.log("myLastIndexOf Test Results:");
     console.log(arr.lastIndexOf(4));
     console.log(arr.myLastIndexOf(4));
+    console.log();
 
     const details = {
         name: 'John Doe',
@@ -173,12 +270,17 @@ function main() {
     };
 
     //test for Object.keys()
+    console.log("grabKeys Test Results:");
     console.log(Object.keys(details));
     console.log(grabKeys(details));
+    console.log();
 
     //test for Object.values()
+    console.log("grabValues Test Results:");
     console.log(Object.values(details));
     console.log(grabValues(details));
+    console.log();
+    
 };
 
 main();
