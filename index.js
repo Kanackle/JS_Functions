93cf3930986c20a8c28182dd2fae99c17e8f85e3
KanakSomani <kanak.somani02@myhunter.cuny.edu>
2021-02-24 21:36:24 -0500

    testing

--- index.js
+++ index.js
@@ -82,6 +82,16 @@ Array.prototype.myReduce =  function(callbackfn) {
     return retVal;
 }
 
+/*
+f10
+Without using the native “Array.prototype.lastIndexOf” method of JavaScript, 
+compose a function titled “myLastIndexOf” that will take in an array of elements 
+and returns the index of the last encounter of a target element (if it is found) 
+or -1 if that element does not exist within the input array.
+*/
+
+
+
 /**
  * Main function to run tests in
  */
